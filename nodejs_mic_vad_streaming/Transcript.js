ade068ed17b94379cdc311ce7d2a14d1e67d5571
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-27 12:17:26 +0200

    :rocket: perf(nodejs_mic_vad_streaming): Memoize intermediate decodes.

--- nodejs_mic_vad_streaming/Transcript.js
+++ nodejs_mic_vad_streaming/Transcript.js
@@ -1,15 +1,20 @@
+const mem = require('mem');
+
+const maxAge = 400;
+
 class Transcript {
 
 	constructor (pipeline) {
 		this.pipeline = pipeline;
 		this.byteLength = 0;
+		this.intermediate = mem(() => this.pipeline.intermediateDecode(), {maxAge});
 	}
 
 	process (chunk) {
 		this.byteLength += chunk.length;
 		const start = new Date();
 		this.pipeline.feedAudioContent(chunk);
-		const text = this.pipeline.intermediateDecode();
+		const text = this.intermediate();
 		const processingTime = new Date() - start;
 		return {
 			text,
